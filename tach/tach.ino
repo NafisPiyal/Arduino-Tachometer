fb7f59a09657a41960938fb89146ac80783784c6
Nafis Piyal <nafispiyal@gmail.com>
2022-07-26 15:08:38 -0500

    Update tach.ino

--- tach/tach.ino
+++ tach/tach.ino
@@ -2,14 +2,13 @@
 #include <Adafruit_GFX.h>  // Include core graphics library for the display.
 #include <Adafruit_SSD1306.h>  // Include Adafruit_SSD1306 library to drive the display.
 Adafruit_SSD1306 display(128, 64);  // Create display.
-#include <Fonts/FreeMonoBold18pt7b.h>  // Add a custom font.
+#include <Fonts/FreeMonoBold12pt7b.h>  // Add a custom font.
 
 int pin = 2; // Hall sensor at pin 2
 
 unsigned long rpm = 0;
-unsigned long duration;
 unsigned long rev = 0;
-
+unsigned long duration;
 
 void revCount() {
   rev++;
@@ -20,7 +19,7 @@ void setup() {
   pinMode(pin, INPUT); //Sets sensor as input
 
   // OLED 0.96" Display:
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // Initialize display with the I2C address of 0x3C.
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // Initialize display with the I2C address of 0x3D.
   display.clearDisplay();  // Clear the buffer.
   display.setTextColor(WHITE);  // Set color of the text to white.
   display.setRotation(0);  // Set orientation. Goes from 0, 1, 2 or 3.
@@ -45,7 +44,7 @@ void loop() {
   Serial.print("RPM ");
   Serial.print(rpm);
 
-  Serial.print(" Rev ");
+  Serial.print(" REV ");
   Serial.print(rev);
 
   Serial.print(" DUR ");
@@ -59,23 +58,24 @@ void loop() {
   dtostrf(rpm, 6, 0, string);  // (<variable>,<amount of digits we are going to use>,<amount of decimal digits>,<string name>)
 
   display.clearDisplay();  // Clear the display so we can refresh.
-  display.setTextSize(3);  // Set text size. We are using a custom font so you should always use the text size of 0.
-  display.setCursor(0, 5);  // (x,y).
+  display.setFont(&FreeMonoBold12pt7b);  // Set a custom font.
+  display.setTextSize(0);  // Set text size. We are using a custom font so you should always use the text size of 0.
+  display.setCursor(0, 20);  // (x,y).
   display.println("RPM:");  // Text or value to print.
 
   // Print variable with right alignment:
-  display.setCursor(20, 5);  // (x,y).
+  display.setCursor(45, 20);  // (x,y).
   display.println(string);  // Text or value to print.
 
   char string2[10];  // Create a character array of 10 characters
   // Convert float to a string:
   dtostrf(rev, 6, 0, string2);  // (<variable>,<amount of digits we are going to use>,<amount of decimal digits>,<string name>)
-  display.setTextSize(3);  // Set text size. We are using a custom font so you should always use the text size of 0.
-  display.setCursor(0, 35);  // (x,y).
+  display.setTextSize(0);  // Set text size. We are using a custom font so you should always use the text size of 0.
+  display.setCursor(0, 52);  // (x,y).
   display.println("REV:");  // Text or value to print.
 
   // Print variable with right alignment:
-  display.setCursor(20, 35);  // (x,y).
+  display.setCursor(45, 52);  // (x,y).
   display.println(string2);  // Text or value to print.
 
   display.display();
